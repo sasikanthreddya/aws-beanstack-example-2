2a1d3cb1a0c3b7b940d07041265d49ad4a32d7c5
sasikanthreddya <sasi.tech14@gmail.com>
2020-06-02 13:20:29 +0530

    added test method

--- src/main/java/com/aws/beanstack/AwsBeanstackApplication.java
+++ src/main/java/com/aws/beanstack/AwsBeanstackApplication.java
@@ -2,12 +2,20 @@ package com.aws.beanstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AwsBeanstackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsBeanstackApplication.class, args);
 	}
-
+	
+	@GetMapping("/test")
+public String testController() {
+	return "testing";
+	
+}
 }
